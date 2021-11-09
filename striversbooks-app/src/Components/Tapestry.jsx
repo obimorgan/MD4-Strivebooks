a07c30cb12c4bb5cf3728008248cf537aa916ad7
Roby Morgan <roby.c.morgan@gmail.com>
2021-11-09 17:41:37 +0100

    resized the tapestry imgs.

--- striversbooks-app/src/Components/Tapestry.jsx
+++ striversbooks-app/src/Components/Tapestry.jsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import Card from "react-bootstrap/Card";
-import { Container, Button, Row, Col, H1 } from "react-bootstrap";
+import { Container, Row, Col,} from "react-bootstrap";
 import items from "../Data/scify.json";
 
 // class Tapestry extends Component {
@@ -22,7 +22,7 @@ import items from "../Data/scify.json";
 //                         <Card.Body>
 //                         <Card.Title
 //                         className="mx-n3 my-n2"
-                        
+
 //                         >
 //                             {item.title}
 //                         </Card.Title>
@@ -42,32 +42,24 @@ import items from "../Data/scify.json";
 
 const Tapestry = (props) => (
   <Container>
-        <h1>Latest Release</h1>
+    <h1>Latest Release</h1>
     <Row>
       <Col className="d-flex flex-wrap">
-        
         {items.map((item) => (
-          <Card style={{ width: "12rem",}}
-          className="col-xs-3 col-sm-4 col-md-3 px-n1"
+          <Card
+            style={{ width: "10rem", height: "15rem"}}
+            className="px-n1"
+            key={item.asin}
           >
-            <Card.Img
-              variant="top"
-              src={item.img}
-              className="img-fluid"
-            />
-            <Card.Body>
-              <Card.Title
-              className="mx-n3 my-n2"
-              
-              >
-                  {item.title}
-              </Card.Title>
+            <Card.Img variant="top" src={item.img} className="img-fluid" />
+            {/* <Card.Body> */}
+              {/* <Card.Title className="mx-n3 my-n2">{item.title}</Card.Title> */}
               {/* <Card.Text>
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </Card.Text> */}
               {/* <Button variant="primary">Go somewhere</Button> */}
-            </Card.Body>
+            {/* </Card.Body> */}
           </Card>
         ))}
       </Col>
