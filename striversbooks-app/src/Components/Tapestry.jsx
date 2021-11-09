fd524b35d1236c35be0243048bc8c440ac766247
Roby Morgan <roby.c.morgan@gmail.com>
2021-11-09 17:52:54 +0100

    changed tapestry from being a fnc to a class.

--- striversbooks-app/src/Components/Tapestry.jsx
+++ striversbooks-app/src/Components/Tapestry.jsx
@@ -3,67 +3,71 @@
 import Card from "react-bootstrap/Card";
 import { Container, Row, Col,} from "react-bootstrap";
 import items from "../Data/scify.json";
+import {Component} from "react"
 
-// class Tapestry extends Component {
-//     render() {
-//         <Container>
-//             <Row>
-//                 <Col className="d-flex flex-wrap">
-//                     {items.map((item) => (
-//                     <Card style={{ width: "12rem",}}
-//                     className="col-xs-3 col-sm-4 col-md-3 px-n1"
-//                     key={item.id}
-//                     >
-//                         <Card.Img
-//                         variant="top"
-//                         src={item.img}
-//                         className="img-fluid"
-//                         />
-//                         <Card.Body>
-//                         <Card.Title
-//                         className="mx-n3 my-n2"
+class Tapestry extends Component {
+    render() {
+        return(
+            <Container>
+            <Row>
+                <Col className="d-flex flex-wrap">
+                    {items.map((item) => (
+                    <Card style={{ width: "12rem",}}
+                    className="col-xs-3 col-sm-4 col-md-3 px-n1"
+                    key={item.id}
+                    >
+                        <Card.Img
+                        variant="top"
+                        src={item.img}
+                        className="img-fluid"
+                        />
+                        <Card.Body>
+                        <Card.Title
+                        className="mx-n3 my-n2"
 
-//                         >
-//                             {item.title}
-//                         </Card.Title>
-//                         {/* <Card.Text>
-//                             Some quick example text to build on the card title and make up
-//                             the bulk of the card's content.
-//                         </Card.Text> */}
-//                         {/* <Button variant="primary">Go somewhere</Button> */}
-//                         </Card.Body>
-//                     </Card>
-//                     ))}
-//                 </Col>
-//             </Row>
-//         </Container>
-//     }
-// }
+                        >
+                            {item.title}
+                        </Card.Title>
+                        {/* <Card.Text>
+                            Some quick example text to build on the card title and make up
+                            the bulk of the card's content.
+                        </Card.Text> */}
+                        {/* <Button variant="primary">Go somewhere</Button> */}
+                        </Card.Body>
+                    </Card>
+                    ))}
+                </Col>
+            </Row>
+        </Container>
+        )
+        
+    }
+}
 
-const Tapestry = (props) => (
-  <Container>
-    <h1>Latest Release</h1>
-    <Row>
-      <Col className="d-flex flex-wrap">
-        {items.map((item) => (
-          <Card
-            style={{ width: "10rem", height: "15rem"}}
-            className="px-n1"
-            key={item.asin}
-          >
-            <Card.Img variant="top" src={item.img} className="img-fluid" />
-            {/* <Card.Body> */}
-              {/* <Card.Title className="mx-n3 my-n2">{item.title}</Card.Title> */}
-              {/* <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text> */}
-              {/* <Button variant="primary">Go somewhere</Button> */}
-            {/* </Card.Body> */}
-          </Card>
-        ))}
-      </Col>
-    </Row>
-  </Container>
-);
+// const Tapestry = (props) => (
+//   <Container>
+//     <h1>Latest Release</h1>
+//     <Row>
+//       <Col className="d-flex flex-wrap">
+//         {items.map((item) => (
+//           <Card
+//             style={{ width: "10rem", height: "15rem"}}
+//             className="px-n1"
+//             key={item.asin}
+//           >
+//             <Card.Img variant="top" src={item.img} className="img-fluid" />
+//             {/* <Card.Body> */}
+//               {/* <Card.Title className="mx-n3 my-n2">{item.title}</Card.Title> */}
+//               {/* <Card.Text>
+//                 Some quick example text to build on the card title and make up
+//                 the bulk of the card's content.
+//               </Card.Text> */}
+//               {/* <Button variant="primary">Go somewhere</Button> */}
+//             {/* </Card.Body> */}
+//           </Card>
+//         ))}
+//       </Col>
+//     </Row>
+//   </Container>
+// );
 export default Tapestry;
