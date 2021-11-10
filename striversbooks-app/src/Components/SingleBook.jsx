11ed596a4e82fe53420a6475bfd508d152ac685a
Roby Morgan <roby.c.morgan@gmail.com>
2021-11-10 23:31:43 +0100

    added ex 6 solution/add stiling on click event.

--- striversbooks-app/src/Components/SingleBook.jsx
+++ striversbooks-app/src/Components/SingleBook.jsx
@@ -3,22 +3,28 @@ import Card from "react-bootstrap/Card";
 import React, { Component } from "react";
 
 class SingleBook extends Component {
-  render() {
-      return (
-        <div>
-         <Card className="book-cover d-flex">
-          <Card.Img variant="top" src={this.props.bookInfo.img} />
-          <Card.Body>
-            <Card.Title>{this.props.bookInfo.title}</Card.Title>
-            <Card.Text>
-              {this.props.bookInfo.category}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
-      )
+    state = {
+        selected: false
+    }
+    render() {
+        return (
+            <div>
+                <Card className="book-cover d-flex"
+                onClick={(e) => this.setState({ selected: !this.state.selected})}
+                style={{ border: this.state.selected ? "3px solid red" : "none"}}
+                >
+                <Card.Img variant="top" src={this.props.bookInfo.img} />
+                <Card.Body>
+                    <Card.Title>{this.props.bookInfo.title}</Card.Title>
+                    <Card.Text>
+                    {this.props.bookInfo.category}
+                    </Card.Text>
+                </Card.Body>
+                </Card>
+            </div>
+        )
 
-  }
+    }
 }
 
 export default SingleBook
